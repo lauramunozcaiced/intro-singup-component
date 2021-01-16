2b9e7ff7f2bd1682a4f7334ef4a02d5f7946cb1f
Laura Muñoz Caicedo <64536735+lauramunozcaiced@users.noreply.github.com>
2021-01-16 11:54:41 -0500

    Update functions-personalized-buyclick.js

--- docs/js/functions-personalized-buyclick.js
+++ docs/js/functions-personalized-buyclick.js
@@ -1,5 +1,12 @@
 jQuery('document').ready(function(){
   
-  console.log(jQuery('.elementor-menu-cart__wrapper dl.variation'));
+  jQuery('.elementor-menu-cart__wrapper .elementor-menu-cart__product.woocommerce-cart-form__cart-item.cart_item').each(function(){
+    jQuery('table.wc-block-cart-items tbody').append('<h3>'+jQuery(this).children('.wcfm_dashboard_item_title').text()+'</h3>');
+  });
+  
+  
+  //var product = jQuery('.elementor-menu-cart__wrapper .elementor-menu-cart__product.woocommerce-cart-form__cart-item.cart_item');
+  
+  //title = product.children('.elementor-menu-cart__product-name.product-name')
   
 });
