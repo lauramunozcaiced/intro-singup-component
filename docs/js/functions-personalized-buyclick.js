ef998192bc0baa0d93844819276dbc74a8745919
Laura Muñoz Caicedo <64536735+lauramunozcaiced@users.noreply.github.com>
2021-01-18 00:03:06 -0500

    Update functions-personalized-buyclick.js

--- docs/js/functions-personalized-buyclick.js
+++ docs/js/functions-personalized-buyclick.js
@@ -1,5 +1,8 @@
 jQuery('document').ready(function(){
 		
+	//jQuery('.woocommerce-form.woocommerce-form-register.register .woocommerce-privacy-policy-text').append('<div id="privacy_policy" style="display: flex;"><input type="checkbox" name="privacy" required></div>')
+	//jQuery('.woocommerce-form.woocommerce-form-register.register p:nth-child(4)').appendTo('#privacy_policy');
+	
 	jQuery('.owp-cart-overlay').append('<div class="message_add_product"><h3>Se ha agregado un producto en el carrito</h3></div>');
 	jQuery('.wc-block-components-order-summary-item .wc-block-components-order-summary-item__total-price').each(function(){
 		var text = jQuery(this).text().replace('<span class="wcpdf-currency-symbol">','');
